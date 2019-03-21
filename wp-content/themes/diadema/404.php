3468c29dcfb970976a7bae90b1d05b83d9a9e463
Mathews Wednesday <q3dm0@yahoo.com>
2019-03-21 16:12:04 +0300

    Правки

--- wp-content/themes/diadema/404.php
+++ wp-content/themes/diadema/404.php
@@ -21,7 +21,6 @@ get_header();
             <div class="row">
                 <div class="col-12 col-md-6 col-md-push-3 col-sm-6 col-sm-push-3">
                     <div class="page-404">
-                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/Munk@2x.jpg" alt="">
                         <h1>Ошибка 404</h1>
                         <h3>Данная страница не найдена</h3>
                         <a href="<?php echo esc_url(home_url('/')); ?>" class="light-button">На главную</a>
