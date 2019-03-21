7c35b0c45835f3611229ea8b17ab00d37cc00e6a
Anatoliy <anatoliy@sixpotatoes.com>
2019-03-21 14:30:25 +0200

    Fix - v3

--- wp-content/themes/diadema/404.php
+++ wp-content/themes/diadema/404.php
@@ -8,10 +8,10 @@ Description: Тема Psychologue
 Version: 1.0
 */
 
-get_header(); 
+get_header();
 ?>
 
-    <section>
+    <div class="section-404">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
@@ -19,16 +19,17 @@ get_header();
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6 col-md-push-3 col-sm-6 col-sm-push-3">
+                <div class="col-12 col-md-6 col-md-push-3 col-sm-6 col-sm-push-3">
                     <div class="page-404">
-                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/Munk@2x.jpg" alt="">
-                        <p class="title">Ошибка 404</p>
-                        <p>Данная страница не найдена</p>
-                        <p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="light-button">На главную</a></p>
+                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/Munk@2x.jpg" alt="">
+                        <h1>Ошибка 404</h1>
+                        <h3>Данная страница не найдена</h3>
+                        <a href="<?php echo esc_url(home_url('/')); ?>" class="light-button">На главную</a>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+
 
 <?php get_footer(); ?>
